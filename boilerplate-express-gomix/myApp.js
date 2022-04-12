cff73f134ac64d3bb7da0a4820f9986717a22b97
Emma King <emmailonaking@gmail.com>
2022-04-12 15:14:03 +0300

    express functions

---
+++ boilerplate-express-gomix/myApp.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let app = express();
+
+let absolutePath = __dirname + "/views/index.html";
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
+
+
+app.use("/public", express.static(__dirname + "/public"));
+
+
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
